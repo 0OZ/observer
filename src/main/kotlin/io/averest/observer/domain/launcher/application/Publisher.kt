0a580438a3cbbe023ebbdbff344f996d682c2349
Christian <matschke.berlin@icloud.com>
2021-05-17 09:49:22 +0200

    Update Publisher.kt
    ** own Thread context

--- src/main/kotlin/io/averest/observer/domain/launcher/application/Publisher.kt
+++ src/main/kotlin/io/averest/observer/domain/launcher/application/Publisher.kt
@@ -2,20 +2,21 @@ package io.averest.observer.domain.launcher.application
 
 import io.averest.observer.domain.launcher.aggregate.Issuer
 import io.averest.observer.infrastructure.Designator
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.apache.logging.log4j.LogManager
+import kotlin.coroutines.coroutineContext
 
 class Publisher(
     private val callable: Designator,
     private val identifier: Issuer = Issuer(callable.javaClass.simpleName, callable.identifier),
 ) {
     private val logger = LogManager.getFormatterLogger(this.javaClass)
+
+    @ObsoleteCoroutinesApi
     fun commit() {
         if (identifier.getJob() == null || identifier.getJob()!!.isCompleted) {
-            val job = GlobalScope.launch(Dispatchers.IO) { callable.launchJob() }
+            val context = newSingleThreadContext(identifier.ident)
+            val job = GlobalScope.launch(context) { callable.launchJob() }
             identifier.setJob(job)
             logger.info("${identifier.type}${identifier.ident} subscribe ✓ ${job.isActive}")
         }
